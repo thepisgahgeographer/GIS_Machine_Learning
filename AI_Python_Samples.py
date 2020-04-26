eff3b27feac4dbd9bc9d7e6c68c4ec4c62f9b428
Yuri Potawsky <potawsky.yuri@gmail.com>
2020-04-25 22:11:02 -0400

    Jupyter

--- AI_Python_Samples.py
+++ AI_Python_Samples.py
@@ -1,6 +1,39 @@
 from pathlib import Path
+import fastai
+from fastai import utils
+import platform
+
 
 #Working with PurePaths through pathlib
 p = Path("C:\GIS_Data")
 #print([x for x in p.iterdir() if x.is_dir()])
-print(list(p.glob('**/*.las')))
+#print(list(p.glob('**/*.las')))
+
+#print(platform)
+#print('this is version {}.'.format(platform.python_version()))
+
+Bike = {
+    "color": "Gunmetal",
+    "Frame": "Large",
+    "travel": 160
+}
+
+for index, val in enumerate(Bike):
+    print("Index: {}".format(index))
+    print(val)
+
+for vals in range(1,10):
+    for values in range(1,10):
+        print("{} X {} = {}".format(vals, values, vals*values))
+
+#print(Bike["travel"])
+
+#if "travel" in Bike:
+#    print("True")
+
+#print("5"*10)
+
+#for letter in "Wilson Creek":
+#    print(letter, end=':')
+
+
